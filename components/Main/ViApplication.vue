f817c8298fe23b5f68ac3b85b7c2bf1230961ca6
m1rstan1978 <m1rstan@mail.ru>
2024-02-22 21:26:00 +0300

    fadf

--- components/Main/ViApplication.vue
+++ components/Main/ViApplication.vue
@@ -1,174 +1,195 @@
 <template>
   <section class="application">
     <div class="application__container">
-        <div class="application__info">
-            <h3 class="application__title">Оставьте заявку и мы вам перезвоним</h3>
-            <p class="application__description">наши специалисты ответят на все вопросы и смогут подобрать нужную программу</p>
+      <div class="application__info">
+        <h3 class="application__title">Оставьте заявку и мы вам перезвоним</h3>
+        <p class="application__description">
+          наши специалисты ответят на все вопросы и смогут подобрать нужную
+          программу
+        </p>
+      </div>
+      <div class="application__form">
+        <div class="application__data">
+          <input class="application__input" type="text" placeholder="Имя" />
+          <input
+            class="application__input"
+            type="text"
+            placeholder="+7 (000) - 000 - 00 - 00"
+            v-model="isNumber"
+            @beforeinput="handleBeforeInput"
+            @input="numberValidator"
+          />
         </div>
-        <div class="application__form">
-            <div class="application__data">
-                <input class="application__input" type="text" placeholder="Имя">
-                <input class="application__input" type="text" placeholder="+7 (000) - 000 - 00 - 00" v-model="isNumber" @beforeinput="handleBeforeInput" @input="numberValidator">
-            </div>
-            <button class="application__btn">Отправить</button>
-            <p class="application__policy">*Нажимая кнопку вы соглашаетесь на обработку персональных данных на условиях, определенных <span class="application__policy_special">Политикой конфиденциальности</span></p>
-        </div>
+        <button class="application__btn">Отправить</button>
+        <p class="application__policy">
+          *Нажимая кнопку вы соглашаетесь на обработку персональных данных на
+          условиях, определенных
+          <span class="application__policy_special"
+            >Политикой конфиденциальности</span
+          >
+        </p>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            isNumber: '',
-            isNumberPrev: ''
-        }
+  data() {
+    return {
+      isNumber: "",
+      isNumberPrev: "",
+    };
+  },
+  methods: {
+    handleBeforeInput() {
+      this.isNumberPrev = this.isNumber;
     },
-    methods: {
-        handleBeforeInput() {
-            this.isNumberPrev = this.isNumber
-        },
-        numberValidator(event) {
-            console.log(this.isNumber)
-            const hasLetters = /[a-zA-Zа-яА-Я!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]$/.test(this.isNumber)
-            const cursorPosition = event.target.selectionStart;
+    numberValidator(event) {
+      console.log(this.isNumber);
+      const hasLetters = /[a-zA-Zа-яА-Я!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]$/.test(
+        this.isNumber
+      );
+      const cursorPosition = event.target.selectionStart;
 
-            if (cursorPosition < this.isNumber.length) {
-                this.isNumber = this.isNumberPrev
-            }
+      if (cursorPosition < this.isNumber.length) {
+        this.isNumber = this.isNumberPrev;
+      }
 
-            if (hasLetters) {
-                this.isNumber = this.isNumber.replace(/[a-zA-Zа-яА-Я!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]$/, '');
-            }
+      if (hasLetters) {
+        this.isNumber = this.isNumber.replace(
+          /[a-zA-Zа-яА-Я!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]$/,
+          ""
+        );
+      }
 
-            if (this.isNumber[0] !== '+') {
-                this.isNumber = "+7 (" + this.isNumber
-            }
-            if (this.isNumber.length === 3) {
-                this.isNumber = ''
-            }
-            if (this.isNumber.length === 7) {
-                this.isNumber = this.isNumber + ') - '
-            }
-            if (this.isNumber.length === 9) {
-                this.isNumber = this.isNumber.slice(0, -3)
-            }
-            if (this.isNumber.length === 14) {
-                this.isNumber = this.isNumber + ' - '
-            }
-            if (this.isNumber.length === 15) {
-                this.isNumber = this.isNumber.slice(0, -2)
-            }
-            if (this.isNumber.length === 19) {
-                this.isNumber = this.isNumber + ' - '
-            }
-            if (this.isNumber.length === 20) {
-                this.isNumber = this.isNumber.slice(0, -2)
-            }
-            if (this.isNumber.length === 25) {
-                this.isNumber = this.isNumber.slice(0, -1)
-            }
+      if (this.isNumber[0] !== "+") {
+        this.isNumber = "+7 (" + this.isNumber;
+      }
+      if (this.isNumber.length === 3) {
+        this.isNumber = "";
+      }
+      if (this.isNumber.length === 7) {
+        this.isNumber = this.isNumber + ") - ";
+      }
+      if (this.isNumber.length === 9) {
+        this.isNumber = this.isNumber.slice(0, -3);
+      }
+      if (this.isNumber.length === 14) {
+        this.isNumber = this.isNumber + " - ";
+      }
+      if (this.isNumber.length === 15) {
+        this.isNumber = this.isNumber.slice(0, -2);
+      }
+      if (this.isNumber.length === 19) {
+        this.isNumber = this.isNumber + " - ";
+      }
+      if (this.isNumber.length === 20) {
+        this.isNumber = this.isNumber.slice(0, -2);
+      }
+      if (this.isNumber.length === 25) {
+        this.isNumber = this.isNumber.slice(0, -1);
+      }
 
-            if (this.isNumber.length != 0) {
-                this.phoneValidator = 1
-            }
-            if (this.isNumber.length === 24) {
-                this.phoneValidator = 2
-            }
-            if (this.isNumber.length === 0) {
-                this.phoneValidator = 0
-            }
-        }
-    }
-}
+      if (this.isNumber.length != 0) {
+        this.phoneValidator = 1;
+      }
+      if (this.isNumber.length === 24) {
+        this.phoneValidator = 2;
+      }
+      if (this.isNumber.length === 0) {
+        this.phoneValidator = 0;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
 .application {
-    background: #000;
-    padding: 100px 0 100px 0;
+  width: 100vw;
+  background: #000;
+  padding: 100px 0 100px 0;
 }
 .application__container {
-    max-width: 1800px;
-    padding: 0 20px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  max-width: 1800px;
+  padding: 0 20px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .application__title {
-    font-family: "Inter", sans-serif;
-    font-weight: 700;
-    font-size: 80px;
-    max-width: 890px;
-    color: #fff;
-    line-height: 75px;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 80px;
+  max-width: 890px;
+  color: #fff;
+  line-height: 75px;
 }
 .application__description {
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    margin-top: 30px;
-    color: #fff;
-    font-size: 20px;
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  margin-top: 30px;
+  color: #fff;
+  font-size: 20px;
 }
 .application__form {
-    width: 745px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  width: 745px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 .application__data {
-    display: flex;
-    align-items: center;
-    
+  display: flex;
+  align-items: center;
 }
 .application__input {
-    width: 100%;
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    padding: 20px;
-    border-radius: 50px;
-    text-align: center;
-    font-size: 28px;
-    color: var(--violo);
+  width: 100%;
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  padding: 20px;
+  border-radius: 50px;
+  text-align: center;
+  font-size: 28px;
+  color: var(--violo);
 }
 .application__input:nth-child(1) {
-    flex-basis: 40%;
+  flex-basis: 40%;
 }
 .application__input:last-child {
-    transform: translateX(-10px);
-    flex-basis: 60%;
+  transform: translateX(-10px);
+  flex-basis: 60%;
 }
 .application__btn {
-    width: 100%;
-    padding: 20px 0;
-    border: 1px solid #fff;
-    font-weight: 700;
-    font-size: 28px;
-    color: #fff;
-    border-radius: 50px;
-    margin-top: 20px;
-    transition: all .3s ease;
+  width: 100%;
+  padding: 20px 0;
+  border: 1px solid #fff;
+  font-weight: 700;
+  font-size: 28px;
+  color: #fff;
+  border-radius: 50px;
+  margin-top: 20px;
+  transition: all 0.3s ease;
 }
 .application__btn:hover {
-    background: var(--violo);
-    border: 1px solid var(--violo);
+  background: var(--violo);
+  border: 1px solid var(--violo);
 }
 .application__policy {
-    font-family: "Inter", sans-serif;
-    text-align: center;
-    max-width: 670px;
-    color: #fff;
-    font-size: 14px;
-    margin-top: 15px;
+  font-family: "Inter", sans-serif;
+  text-align: center;
+  max-width: 670px;
+  color: #fff;
+  font-size: 14px;
+  margin-top: 15px;
 }
 .application__policy_special {
-    color: var(--violo);
-    font-weight: 500;
-    cursor: pointer;
+  color: var(--violo);
+  font-weight: 500;
+  cursor: pointer;
 }
 .application__policy_special:hover {
-    border-bottom: 1px solid var(--violo);
+  border-bottom: 1px solid var(--violo);
 }
 </style>
--- components/Main/ViNews.vue
+++ components/Main/ViNews.vue
@@ -1,197 +1,230 @@
 <template>
   <section class="news">
     <div class="news__container">
-        <div class="news__roof">
-            <h2 class="news__title">Новости</h2>
-            <button class="news__btn">Все новости
-                <svg class="news__svg" width="55" height="19" viewBox="0 0 66 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M66 9.5L51 0.839743L51 18.1603L66 9.5ZM52.5 8L-6.55671e-08 8L6.55671e-08 11L52.5 11L52.5 8Z" fill="white"/>
-                </svg>
-            </button>
+      <div class="news__roof">
+        <h2 class="news__title">Новости</h2>
+        <button class="news__btn">
+          Все новости
+          <svg
+            class="news__svg"
+            width="55"
+            height="19"
+            viewBox="0 0 66 19"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M66 9.5L51 0.839743L51 18.1603L66 9.5ZM52.5 8L-6.55671e-08 8L6.55671e-08 11L52.5 11L52.5 8Z"
+              fill="white"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="news__content">
+        <div
+          class="news__main"
+          data-aos="fade-right"
+          data-aos-offset="400"
+          data-aos-duration="1000"
+        >
+          <div class="news__main_image">
+            <img class="news__img" :src="ArrayNews[0].image" alt="" />
+            <div class="news__background"></div>
+          </div>
+          <p class="news__info">
+            {{ ArrayNews[0].data }} |
+            <span class="news__type">{{ ArrayNews[0].type }}</span>
+          </p>
+          <h3 class="news__name">{{ ArrayNews[0].name }}</h3>
         </div>
-        <div class="news__content">
-            <div class="news__main" data-aos="fade-right" data-aos-offset="400" data-aos-duration="1000">
-                <div class="news__main_image">
-                    <img class="news__img" :src="ArrayNews[0].image" alt="">
-                    <div class="news__background"></div>
-                </div>
-                <p class="news__info">
-                    {{ ArrayNews[0].data }} | <span class="news__type">{{ ArrayNews[0].type }}</span>
-                </p>
-                <h3 class="news__name">{{ ArrayNews[0].name }}</h3>
+        <div
+          class="news__additionally"
+          data-aos="fade-left"
+          data-aos-offset="400"
+          data-aos-duration="1000"
+        >
+          <div class="news__additionally_add">
+            <div class="news__additionally_image">
+              <img
+                class="news__additionally_img"
+                :src="ArrayNews[1].image"
+                alt=""
+              />
+              <div class="news__background"></div>
             </div>
-            <div class="news__additionally" data-aos="fade-left" data-aos-offset="400" data-aos-duration="1000">
-                <div class="news__additionally_add">
-                    <div class="news__additionally_image">
-                        <img class="news__additionally_img" :src="ArrayNews[1].image" alt="">
-                        <div class="news__background"></div>
-                    </div>
-                    <div>
-                        <p class="news__info news__info_add">
-                            {{ ArrayNews[1].data }} | <span class="news__type">{{ ArrayNews[1].type }}</span>
-                        </p>
-                        <h3 class="news__name">{{ ArrayNews[1].name }}</h3>
-                    </div>
-                </div>
-                <div class="news__additionally_add">
-                    <div class="news__additionally_image">
-                        <img class="news__additionally_img" :src="ArrayNews[2].image" alt="">
-                        <div class="news__background"></div>
-                    </div>
-                    <div>
-                        <p class="news__info news__info_add">
-                            {{ ArrayNews[2].data }} | <span class="news__type">{{ ArrayNews[2].type }}</span>
-                        </p>
-                        <h3 class="news__name">{{ ArrayNews[2].name }}</h3>
-                    </div>
-                </div>
+            <div>
+              <p class="news__info news__info_add">
+                {{ ArrayNews[1].data }} |
+                <span class="news__type">{{ ArrayNews[1].type }}</span>
+              </p>
+              <h3 class="news__name">{{ ArrayNews[1].name }}</h3>
             </div>
+          </div>
+          <div class="news__additionally_add">
+            <div class="news__additionally_image">
+              <img
+                class="news__additionally_img"
+                :src="ArrayNews[2].image"
+                alt=""
+              />
+              <div class="news__background"></div>
+            </div>
+            <div>
+              <p class="news__info news__info_add">
+                {{ ArrayNews[2].data }} |
+                <span class="news__type">{{ ArrayNews[2].type }}</span>
+              </p>
+              <h3 class="news__name">{{ ArrayNews[2].name }}</h3>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            ArrayNews: [
-                {
-                    image: "../assets/images/Main/new.webp",
-                    data: "15 сентября, 2023",
-                    type: "Мероприятия",
-                    name: "Путешествие в Советский район"
-                },
-                {
-                    image: "../assets/images/Main/new.webp",
-                    data: "10 августа, 2023",
-                    type: "Новости колледжа",
-                    name: "Посвящение в студенты"
-                },
-                {
-                    image: "../assets/images/Main/new.webp",
-                    data: "21 мая, 2022",
-                    type: "Новости колледжа",
-                    name: "Дополнительные предметы"
-                },
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      ArrayNews: [
+        {
+          image: "../assets/images/Main/new.webp",
+          data: "15 сентября, 2023",
+          type: "Мероприятия",
+          name: "Путешествие в Советский район",
+        },
+        {
+          image: "../assets/images/Main/new.webp",
+          data: "10 августа, 2023",
+          type: "Новости колледжа",
+          name: "Посвящение в студенты",
+        },
+        {
+          image: "../assets/images/Main/new.webp",
+          data: "21 мая, 2022",
+          type: "Новости колледжа",
+          name: "Дополнительные предметы",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
 .news {
-    padding-top: 60px;
+  width: 100vw;
+  padding-top: 60px;
 }
 .news__container {
-    max-width: 1440px;
-    padding: 0 20px;
-    margin: 0 auto;
+  max-width: 1440px;
+  padding: 0 20px;
+  margin: 0 auto;
 }
 .news__roof {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 .news__title {
-    font-family: "Inter", sans-serif;
-    font-weight: 700;
-    font-size: 60px;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 60px;
 }
 .news__btn {
-    font-family: "Inter", sans-serif;
-    font-weight: 700;
-    font-size: 22px;
-    padding: 20px 40px;
-    background: var(--violo);
-    color: #fff;
-    display: flex;
-    align-items: center;
-    border-radius: 50px;
-    transition: all .3s ease-in-out;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 22px;
+  padding: 20px 40px;
+  background: var(--violo);
+  color: #fff;
+  display: flex;
+  align-items: center;
+  border-radius: 50px;
+  transition: all 0.3s ease-in-out;
 }
 .news__btn:hover {
-    transform: translateX(15px);
+  transform: translateX(15px);
 }
 .news__svg {
-    margin-left: 20px;
+  margin-left: 20px;
 }
 .news__content {
-    margin-top: 50px;
-    display: flex;
+  margin-top: 50px;
+  display: flex;
 }
 .news__main {
-    margin-right: 40px;
-    min-width: 680px;
+  margin-right: 40px;
+  min-width: 680px;
 }
 .news__main_image {
-    position: relative;
-    max-width: 680px;
-    height: 450px;
-    z-index: 2;
-    cursor: pointer;
+  position: relative;
+  max-width: 680px;
+  height: 450px;
+  z-index: 2;
+  cursor: pointer;
 }
 .news__info {
-    margin-top: 30px;
-    font-family: "Inter", sans-serif;
-    font-size: 20px;
-    color: #9B9B9B;
+  margin-top: 30px;
+  font-family: "Inter", sans-serif;
+  font-size: 20px;
+  color: #9b9b9b;
 }
 .news__type {
-    font-family: "Inter", sans-serif;
-    font-size: 20px;
-    color: #8101BD;
+  font-family: "Inter", sans-serif;
+  font-size: 20px;
+  color: #8101bd;
 }
 .news__background {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: #8101BD;
-    z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #8101bd;
+  z-index: 1;
 }
 .news__img {
-    position: relative;
-    transition: all .3s ease-in-out;
-    z-index: 3;
+  position: relative;
+  transition: all 0.3s ease-in-out;
+  z-index: 3;
 }
 .news__img:hover {
-    transform: translate(-10px, -10px);
+  transform: translate(-10px, -10px);
 }
 .news__name {
-    font-family: "Inter", sans-serif;
-    font-size: 35px;
-    margin-top: 10px;
+  font-family: "Inter", sans-serif;
+  font-size: 35px;
+  margin-top: 10px;
 }
 .news__additionally_add {
-    display: flex;
-    padding: 30px 0;
-    border-top: 1px solid #000;
-    border-bottom: 1px solid #000;
+  display: flex;
+  padding: 30px 0;
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
 }
 .news__additionally_add:nth-child(1) {
-    margin-bottom: 30px;
+  margin-bottom: 30px;
 }
 .news__additionally_image {
-    position: relative;
-    width: 260px;
-    min-width: 260px;
-    height: 260px;
-    margin-right: 40px;
+  position: relative;
+  width: 260px;
+  min-width: 260px;
+  height: 260px;
+  margin-right: 40px;
 }
 .news__additionally_img {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    z-index: 3;
-    transition: all .3s ease-in-out;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  z-index: 3;
+  transition: all 0.3s ease-in-out;
 }
 .news__additionally_img:hover {
-    transform: translate(-10px, -10px);
+  transform: translate(-10px, -10px);
 }
 .news__info_add {
-    margin-top: 0;
+  margin-top: 0;
 }
 </style>
