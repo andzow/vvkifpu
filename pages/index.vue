9293fe8da9a4ecad6a82f9b23a2a95be3577055f
m1rstan1978 <m1rstan@mail.ru>
2024-03-01 20:35:32 +0300

    fdas

--- pages/index.vue
+++ pages/index.vue
@@ -10,30 +10,10 @@
 
 <script>
 export default {
-  data() {
-    return {};
-  },
-  mounted() {
-    // const newArr = [];
-    // const findArr = [];
-    // const pages = this.arrPages;
-    // pages.forEach((el) => {
-    //   const splitElName = el.name.replace(/[-]/gi, "");
-    //   const splitElPath = el.path.replace(/[-/]/gi, "");
-    //   if (splitElName !== splitElPath && el.name !== "index") {
-    //     newArr.push(el);
-    //   }
-    // });
-    // newArr.forEach((el) => {
-    //   const splitElPath = el.path.replace(/[-/]/gi, "");
-    //   const findIndex = pages.findIndex(
-    //     (elD) => elD.name.replace(/[-]/gi, "") === splitElPath
-    //   );
-    //   if (findIndex !== -1) {
-    //     findArr.push(findIndex);
-    //   }
-    // });
-    // findArr.forEach((idx) => pages.splice(idx, 1));
+  setup() {
+    definePageMeta({
+      middleware: "role",
+    });
   },
 };
 </script>
