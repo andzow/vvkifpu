88c0b5d80573302be4507f01d17794d902e95b3d
andzow <andreywsaers@gmail.com>
2024-03-01 20:41:54 +0300

    sdfsdfsdf

--- app/router.options.ts
+++ app/router.options.ts
@@ -92,5 +92,10 @@ export default <RouterConfig>{
       path: "/library/additionally",
       component: () => import("~/pages/library/additionally.vue"),
     },
+    {
+      name: "Структура и органы управления ПОУ «ВВКИФПУ»",
+      path: "/information/administration",
+      component: () => import("~/pages/information/administration.vue"),
+    },
   ],
 };
