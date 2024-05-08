ee81b109e4d588b93bddfe337b24c699cdbb28dd
m1rstan1978 <m1rstan@mail.ru>
2024-05-08 14:28:39 +0300

    fdas

--- middleware/role.js
+++ middleware/role.js
@@ -1,12 +1,14 @@
 import { jwtDecode } from "jwt-decode";
-import AuthController from "@/http/controllers/AuthController"
+import AuthController from "@/http/controllers/AuthController";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  try {
     if (process.client) {
-        const auth = await AuthController.cheackAuth()
-        const data = jwtDecode(localStorage.getItem("accessToken"))
-        if (auth !== 200 || data.roles.some(item => item !== "ADMIN")) {
-            return navigateTo('/')
-        }
+      const auth = await AuthController.cheackAuth();
+      const data = jwtDecode(localStorage.getItem("accessToken"));
+      if (auth !== 200 || data.roles.some((item) => item !== "ADMIN")) {
+        return navigateTo("/");
+      }
     }
-})
+  } catch {}
+});
